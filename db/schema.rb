8980ddbe89d17a078107095d68ca8f1e2c0cc6aa
lancewrites <lancewrites77@gmail.com>
2022-06-25 14:36:36 -0700

    finished sightings user stories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_234700) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_19_150340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_234700) do
     t.string "common_name"
     t.string "latin_name"
     t.string "kingdom"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.datetime "date"
+    t.decimal "latitude", precision: 10, scale: 6
+    t.decimal "longitude", precision: 10, scale: 6
+    t.integer "animal_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
